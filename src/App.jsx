10998919631e53293a2855f6f956c6b7f7c1e331
Emran Hossain <107532051+emranhossain197@users.noreply.github.com>
2023-09-13 12:18:07 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import { useEffect } from 'react';
-import WOW from 'wowjs';
 import PortfolieRouter from "./router/PortfolieRouter";
 
 function App() {
-
-  useEffect(() => {
-    new WOW.WOW({
-      live: false
-    }).init();
-  }, [])
 
   return (
     <div >
